dc19c73856358e9eff968506120ae873941ceb32
James Prawdzik <sebaprawdzik@gmail.com>
2022-12-19 11:01:45 -0500

    commit tree fc028b85c6b52ab79bc1cd1571320c59cc021937

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+useEffect(() => {
+  if (timeRemaining === 0) /*condition when time hits 0*/ { 
+    setTimeRemaining(10);
+    onAnswered(false);
+    return; /* exit early with empty return */
+  }
+
+  const timerID = setTimeout(() /*sets up a timeout function*/ => {
+    setTimeRemaining((timeRemaining) => timeRemaining -1)
+  }, 1000) /*timeout function will run after 1 second*/
+
+/* cleanup function*/
+  return function () {
+  clearTimeout(timerID);
+  };
+}, [timeRemaining, onAnswered]);
+// the effect needs to run when the timeRemaining changes or when a question is answered
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
